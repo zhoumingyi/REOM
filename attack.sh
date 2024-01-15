dc1476d14d256e0dd69f24e638a84225f7eef5ab
Mingyi Zhou <zhoumingyigege@gmail.com>
2024-01-15 18:49:50 +1100

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -55,8 +55,8 @@ python attack.py  --adv=BIM --model=imagenet --eps=0.01 --nb_iter=400 --eps_iter
 python attack.py  --adv=PGD --model=imagenet --eps=0.01 --nb_iter=400 --eps_iter=0.0001 --target | tee -a acc_asr.txt
 python attack.py  --adv=BIM --model=imagenet --eps=0.1 --nb_iter=400 --eps_iter=0.001 --target | tee -a acc_asr.txt
 python attack.py  --adv=PGD --model=imagenet --eps=0.1 --nb_iter=400 --eps_iter=0.001 --target | tee -a acc_asr.txt
-python attack.py  --adv=BIM --model=imagenet --eps=1.0 --nb_iter=600 --eps_iter=0.05 --target | tee -a acc_asr.txt
-python attack.py  --adv=PGD --model=imagenet --eps=1.0 --nb_iter=600 --eps_iter=0.05 --target | tee -a acc_asr.txt
+python attack.py  --adv=BIM --model=imagenet --eps=1.0 --nb_iter=600 --eps_iter=0.06 --target | tee -a acc_asr.txt
+python attack.py  --adv=PGD --model=imagenet --eps=1.0 --nb_iter=600 --eps_iter=0.06 --target | tee -a acc_asr.txt
 
 
 echo "************************************************************" | tee -a acc_asr.txt
